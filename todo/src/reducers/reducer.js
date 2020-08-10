8bee8e7707d2f2842482354eb84395888c148c59
Galo Sandoval <galosan92@gmail.com>
2020-08-10 14:04:41 -0700

    created needed files and folders

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,14 @@
+const { useReducer } = require("react");
+
+const initialValues = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589,
+};
+
+reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
