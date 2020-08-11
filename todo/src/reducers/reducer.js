50b6550ba72ace8200bb7aced27ab3f3add55179
Galo Sandoval <galosan92@gmail.com>
2020-08-10 17:54:54 -0700

    fixed bug on reducer

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,34 +1,39 @@
 export const initialValues = [
-  {
-    item: "Learn about reducers",
-    completed: false,
-    id: 3892987589,
-  },
+  (todos: [
+    {
+      item: "Learn about reducers",
+      completed: false,
+      id: 3892987589,
+    }
+  ]),
 ];
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
-      console.log(state);
-      return [
+      const newTask = {
+        item: action.payload.id,
+        completed: false,
+        id: new Date.now(),
+      };
+      return {
         ...state,
-        {
-          item: action.payload.id,
-          completed: false,
-          id: new Date.now(),
-        },
-      ];
+        todos: [...state.todos, newTask],
+      };
     case "TOGGLE_ITEM":
-      return state.map((task, id) => {
-        if (task.id === id) {
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        } else {
-          return task;
-        }
-      });
+      return {
+        ...state,
+        todos: state.map((task, id) => {
+          if (task.id === action.payload) {
+            return {
+              ...task,
+              completed: !task.completed,
+            };
+          } else {
+            return task;
+          }
+        }),
+      };
     case "CLEAR_ITEM":
       return state.filter((item) => {
         // eslint-disable-next-line no-unused-expressions
